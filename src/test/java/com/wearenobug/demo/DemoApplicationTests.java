2f6fffde56bf7c193a20457b9555322da03601dc
yangjian <jian.yang@valueonline.cn>
2019-11-22 14:08:18 +0800

    首次提交

---
+++ src/test/java/com/wearenobug/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wearenobug.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
